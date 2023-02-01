33b97ed93a75489d3cdd9a5cff3e310caac92b3b
mehkey <87057470+mehkey@users.noreply.github.com>
2023-02-01 13:22:42 -0500

    Update TopSalesOverLastWeekTest.java

--- src/test/java/com/mehkey/KafkaStreamsTopSellers/TopSalesOverLastWeekTest.java
+++ src/test/java/com/mehkey/KafkaStreamsTopSellers/TopSalesOverLastWeekTest.java
@@ -31,30 +31,6 @@ public class TopSalesOverLastWeekTest {
     private static final String INPUT_TOPIC = "sales";
     private static final String OUTPUT_TOPIC = "top-sales";
 
-    /*
-    public static DockerComposeContainer environment;
-
-    @BeforeClass
-    public static void setUp() {
-        environment =
-                new DockerComposeContainer(new File("./docker-compose.yml"))
-                        .withExposedService("kafka", 9092, Wait.forListeningPort())
-                        .withExposedService("streams-app", 8080, Wait.forHttp("/health"));
-
-    }*/
-
-    /*
-    @ClassRule
-    public static DockerComposeContainer environment =
-            new DockerComposeContainer(new File("docker-compose.yml"))
-                    .withExposedService("kafka", 9092, Wait.forListeningPort())
-                    .withExposedService("streams-app", 8080, Wait.forHttp("/health"));
-
-    @AfterClass
-    public static void cleanUp() {
-        environment.stop();
-    }*/
-
     @Test
     public void testTopSalesOverLastWeek() {
         try {
@@ -74,11 +50,9 @@ public class TopSalesOverLastWeekTest {
 
             while (numSales < 10) {
                 String category = "category-" + random.nextInt(numCategories);
-                //Sale sale = new Sale(category, random.nextDouble() * 100);
                 String saleString = category +','+ (random.nextLong(1,10000));
                 Long price = random.nextLong(1,10000);
                 System.out.println("sending sale string :" + saleString );
-                //ProducerRecord<String, Long> records =
                 producer.send(new ProducerRecord(INPUT_TOPIC, category, price));
                 numSales++;
                 Thread.sleep(1000);
@@ -87,10 +61,7 @@ public class TopSalesOverLastWeekTest {
             // Set up the consumer to read the records from the output topic
             Properties consumerProps = new Properties();
             consumerProps.put("bootstrap.servers", "localhost:9092");
-            //consumerProps.put("bootstrap.servers", environment.getServiceHost("kafka", 9092));
             consumerProps.put("group.id", "test-consumer");
-            //consumerProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-            //consumerProps.put("value.deserializer", "com.mehkey.KafkaStreamsTopSellers.SaleSerde");
             consumerProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
             consumerProps.put("value.deserializer", "org.apache.kafka.common.serialization.LongDeserializer");
 
@@ -105,19 +76,9 @@ public class TopSalesOverLastWeekTest {
                 System.out.println("number of Records Found: "+ records.count());
                 for (ConsumerRecord<String, Long> record : records) {
 
-                    //String[] tokens = record.value().split(",");
-                    //String category = tokens[0];
-                    //Long price = Long.parseLong(tokens[1]);
 
-                    //String expectedCategory = record.key();
-                    //Sale expectedSale = record.value();
-                    //System.out.println("category:" + category+ " sales:"+price);
                     System.out.println("category:" + record.key()+ " sales:"+record.value());
 
-                    //System.out.println("category:" + category+ " sales:"+price);
-
-                    //assertEquals(category, category);
-                    // Add additional checks here to validate the top-sales-over-last-week results
                 }
             }
             System.out.println("FINITO");
